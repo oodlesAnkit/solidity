cddf836a5716c6373fafa6390d84b9444e838dd6
oodlesAnkit <ankit.oodles@gmail.com>
2024-09-06 15:45:31 +0530

    Wethio - WUSD ZRC20 contract

--- contracts/Hish/WBUSD/contarct_ZRC20.sol
+++ contracts/Hish/WBUSD/contarct_ZRC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity 0.4.24;
 
 library SafeMath {
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -19,15 +19,15 @@ library SafeMath {
 
 interface ZRC20 {
     function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external returns (uint256);
-    function transfer(address account, uint256 amount) returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address account, uint256 amount) external   returns (bool);
 
     function transferFrom(
-        address from,
-        address to,
+        address sender,
+        address recipient,
         uint256 amount
-    ) returns (bool);
-    function allowance(address owner, address sender) external returns (bool);
+    ) external returns (bool);
+    function allowance(address owner, address sender) external returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
 
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -43,13 +43,14 @@ contract WUSD is ZRC20 {
 
     string public name = "WethioUSD";
     string public symbol = "WUSD";
-    uint8 public decimals = 2;
+    uint256 public decimals = 2;
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
+    mapping(address=>mapping (address=>uint256)) private _allowances;
     address public owner;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    // event Transfer(address indexed from, address indexed to, uint256 value);
     event Mint(address indexed to, uint256 value);
     event Burn(address indexed from, uint256 value);
 
@@ -65,16 +66,30 @@ contract WUSD is ZRC20 {
         emit Transfer(address(0), owner, totalSupply);
     }
 
+
+    function totalSupply() public view returns (uint256){
+        return totalSupply;
+    }
+
+    function balanceOf(address account) public view returns(uint256){
+        return balanceOf[account];
+    }
+
+
+    function _transfer(address spender,address recipient, uint256 amount) internal {
+        require(balanceOf[spender] >= amount, "Insufficient balance");
+            require(recipient != address(0), "Invalid address");
+
+            balanceOf[spender] = balanceOf[spender].sub(amount);
+            balanceOf[recipient] = balanceOf[recipient].add(amount);
+            emit Transfer(spender, recipient, amount);
+    } 
+
     function transfer(
         address _to,
         uint256 _value
     ) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-        require(_to != address(0), "Invalid address");
-
-        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
-        balanceOf[_to] = balanceOf[_to].add(_value);
-        emit Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender,_to,_value);
         return true;
     }
 
@@ -100,6 +115,30 @@ contract WUSD is ZRC20 {
         totalSupply = totalSupply.sub(amount);
         emit Burn(msg.sender, amount);
         emit Transfer(msg.sender, address(0), amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender)view  returns  (uint256){
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool){
+        _approve(msg.sender,spender,amount);
+        return  true;
+    }
+
+    function _approve(address owner,address spender, uint256 amount) internal {
+        require(owner!=address(0), "ZRC20: approve from the zero address");
+        require(spender!=address(0), "ZRC20: approve from the zero address");
+
+        _allowances[owner][spender]=amount;
+        emit Approval(owner, spender, amount);
+    }
+
+
+    function transferFrom(address sender, address recipient,uint256 amount) public returns(bool){
+        _transfer(sender,recipient,amount);
+        _allowances[sender][recipient].sub(amount);
         return true;
     }
 }
